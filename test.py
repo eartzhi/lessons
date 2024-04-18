789abf53f5109c1350264d63e204eb4a83db0911
eartzhi <zhigunovam@gmail.com>
2024-04-18 15:34:43 +0300

    lesson_10_4

--- test.py
+++ test.py
@@ -1,11 +1,30 @@
 import pandas as pd
+import numpy as np
 
-print(pd.__name__)
+calc = pd.read_excel('data/calc.xlsx')
 
-countries_df = pd.DataFrame({
-    'country': ['Англия', 'Канада', 'США', 'Россия', 'Украина', 'Беларусь', 'Казахстан'],
-    'population': [56.29, 38.05, 322.28, 146.24, 45.5, 9.5, 17.04],
-    'area': [133396, 9984670, 9826630, 17125191, 603628, 207600, 2724902]
-})
+# print(calc.shape)
 
-countries_df.to_excel('data/countries.xlsx', index=False,)
+rand=pd.DataFrame(np.random.uniform(0.85, 1.15, size=calc.shape))
+chamomile=np.random.randint(1, 3)
+print(chamomile)
+calc['chamomile_'+str(chamomile)] = calc['needs']*rand[0]
+calc['chamomile_'+str(3-chamomile)] = 3*calc['needs']-calc['chamomile_'+str(chamomile)]-calc['Itk']
+result=(calc['chamomile_1'].sum()+calc['chamomile_2'].sum()+calc['Itk'].sum())/3
+print(calc)
+print(calc['needs'].sum())
+print(result)
+calc.to_excel('data/result.xlsx')
+# calc.drop(['chamomile_2', 'chamomile_1'], axis=1, inplace=True)
+# print(calc)
+# calc['chamomile_1'] = calc['needs']*np.random.randint(0.85, 1.15)
+# while True:
+#     calc['chamomile_1'] = calc['needs']*np.random.randint(0.85, 1.15)
+#     calc['chamomile_2'] = calc['needs']*np.random.uniform(0.85, 1.15)
+#     result=(calc['chamomile_1'].sum()+calc['chamomile_2'].sum()+calc['Itk'].sum())/3
+#     print(round(result), round(calc['needs'].sum()))
+#     if round(result)==round(calc['needs'].sum()):
+#         print(calc)
+#         break
+#     else:
+#         calc.drop(['chamomile_2', 'chamomile_1'], axis=1, inplace=True)
