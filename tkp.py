1d181c1435ab9650fbe15cd35ba082d052bca699
eartzhi <zhigunovam@gmail.com>
2024-05-13 13:14:21 +0300

    lesson 16.7

--- tkp.py
+++ tkp.py
@@ -1,30 +1,24 @@
 import pandas as pd
 import numpy as np
 
+def check(df):
+    first = df['Инфраструктура'].sum()<df['Ромашка 1'].sum()
+    second = df['Инфраструктура'].sum()<df['Ромашка 2'].sum()
+    return first and second
+
 calc = pd.read_excel('data/calc.xlsx')
 
-# print(calc.shape)
-
-rand=pd.DataFrame(np.random.uniform(0.85, 1.15, size=calc.shape))
+calc['Требуется'] = round(calc['Инфраструктура']*np.random.uniform(1.01, 1.05), 2)
 chamomile=np.random.randint(1, 3)
-# print(chamomile)
-calc['Ромашка '+str(chamomile)] = round(calc['Требуется']*rand[0], 2)
-calc['Ромашка '+str(3-chamomile)] = round(3*calc['Требуется']-calc['Ромашка '+str(chamomile)]-calc['Инфраструктура'], 2)
-result=(calc['Ромашка 1'].sum()+calc['Ромашка 2'].sum()+calc['Инфраструктура'].sum())/3
-# print(calc)
+while True:
+    rand=pd.DataFrame(np.random.uniform(0.85, 1.15, size=calc.shape))
+    calc['Ромашка '+str(chamomile)] = round(calc['Требуется']*rand[0], 2)
+    calc['Ромашка '+str(3-chamomile)] = round(3*calc['Требуется']-calc['Ромашка '+str(chamomile)]-calc['Инфраструктура'], 2)
+    # result=(calc['Ромашка 1'].sum()+calc['Ромашка 2'].sum()+calc['Инфраструктура'].sum())/3
+    if check(calc):
+        break
+
 print(calc['Требуется'].sum())
-print(result)
-calc.to_excel('data/result.xlsx')
-# calc.drop(['chamomile_2', 'chamomile_1'], axis=1, inplace=True)
-# print(calc)
-# calc['chamomile_1'] = calc['needs']*np.random.randint(0.85, 1.15)
-# while True:
-#     calc['chamomile_1'] = calc['needs']*np.random.randint(0.85, 1.15)
-#     calc['chamomile_2'] = calc['needs']*np.random.uniform(0.85, 1.15)
-#     result=(calc['chamomile_1'].sum()+calc['chamomile_2'].sum()+calc['Itk'].sum())/3
-#     print(round(result), round(calc['needs'].sum()))
-#     if round(result)==round(calc['needs'].sum()):
-#         print(calc)
-#         break
-#     else:
-#         calc.drop(['chamomile_2', 'chamomile_1'], axis=1, inplace=True)
+calc.loc[ len(calc.index )] = ['Итого', calc['Инфраструктура'].sum(), calc['Требуется'].sum(), calc['Ромашка '+str(chamomile)].sum(), calc['Ромашка '+str(3-chamomile)].sum()]
+
+calc.to_excel('data/result.xlsx', index=False)
