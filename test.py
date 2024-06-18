dde5204871b6cb7e656942968c363ccde9ae012d
eartzhi <zhigunovam@gmail.com>
2024-06-18 16:57:53 +0300

    eda_3 unit 5 done

--- test.py
+++ test.py
@@ -3,5 +3,6 @@ import pandas as pd
 bronze_top = pd.read_csv('bronze_top.csv')
 silver_top = pd.read_csv('silver_top.csv')
 
-data = bronze_top.merge(silver_top, how='inner', on='Country', suffixes=('bronze', 'silver')) 
+data = bronze_top.merge(silver_top, how='inner', on='Country', 
+                        suffixes=('bronze', 'silver')) 
 print(apply_discounts(products, stocks))
