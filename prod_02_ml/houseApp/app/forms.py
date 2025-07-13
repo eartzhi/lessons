4e4d4e1dc4be118c00a3d6223e0966d9a449a174
eartzhi <zhigunovam@gmail.com>
2025-07-13 22:22:27 +0300

    prod_01_ml done

---
+++ prod_02_ml/houseApp/app/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField,HiddenField
+from wtforms.validators import DataRequired
+
+class EditForm(FlaskForm):
+    id = HiddenField("flatId")
+    title = StringField('Заголовок объявления')
+    city = StringField('Город')
+    rooms = IntegerField('Количество комнат')
+    area = StringField('Площадь')
+    floor = IntegerField('Этаж')
+    cost = IntegerField('Стоимость')
+    submit = SubmitField('Сохранить')
